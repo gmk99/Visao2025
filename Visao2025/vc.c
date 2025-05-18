5d648909f95ac5cc37656bbcf0f8a4bbd1cf0e08
gmk99 <a25664@alunos.ipca.pt>
2025-05-18 18:06:00 +0100

    Adicionar arquivos de projeto.

---
+++ Visao2025/vc.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+#include "vc.h"
+
+IVC* vc_image_new(int width, int height, int channels, int levels)
+{
+    IVC* image = (IVC*)malloc(sizeof(IVC));
+    if (image == NULL)
+        return NULL;
+
+    image->width = width;
+    image->height = height;
+    image->channels = channels;
+    image->levels = levels;
+    image->data = (unsigned char*)malloc(width * height * channels);
+
+    if (image->data == NULL)
+    {
+        free(image);
+        return NULL;
+    }
+
+    return image;
+}
+
+int vc_image_free(IVC* image)
+{
+    if (image != NULL)
+    {
+        if (image->data != NULL)
+            free(image->data);
+        free(image);
+        return 1;
+    }
+    return 0;
+}
+
+
+
+int vc_rgb_get_green(IVC* image)
+{
+    if (image == NULL || image->data == NULL || image->channels != 3)
+        return 0;
+
+    int x, y;
+    for (y = 0; y < image->height; y++)
+    {
+        for (x = 0; x < image->width; x++)
+        {
+            int pos = (y * image->width + x) * 3;
+            image->data[pos + 0] = image->data[pos + 1]; // Red <- Green
+            image->data[pos + 2] = image->data[pos + 1]; // Blue <- Green
+        }
+    }
+    return 1;
+}
---
+++ Visao2025/vc.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef VC_H
+#define VC_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    // Estrutura de imagem
+    typedef struct
+    {
+        unsigned char* data; // Dados da imagem
+        int width, height;   // Dimensões da imagem
+        int channels;        // Número de canais (1=BW, 3=RGB)
+        int levels;          // Níveis de cinzento por canal (por ex: 256)
+    } IVC;
+
+    // Criação de nova imagem
+    IVC* vc_image_new(int width, int height, int channels, int levels);
+
+    // Libertar memória de imagem
+    int vc_image_free(IVC* image);
+
+    // Exemplo de função: extrai o canal verde de uma imagem RGB
+    int vc_rgb_get_green(IVC* image);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VC_H
