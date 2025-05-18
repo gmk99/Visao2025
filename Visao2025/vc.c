3206e51e074185e86acab6d038d22f26f4baea13
FreverZ <carlos94brandao@gmail.com>
2025-05-18 18:37:55 +0100

    V 0.1.1

--- Visao2025/vc.c
+++ Visao2025/vc.c
@@ -2,6 +2,52 @@
 #include <string.h>
 #include "vc.h"
 
+
+/* Structs for coin color ranges */
+typedef struct {
+    int r_min, g_min, b_min;
+    int r_max, g_max, b_max;
+} ColorRange;
+
+typedef struct {
+    char* name;
+    ColorRange range;
+} CoinType;
+
+/* Color ranges for all the different coins */
+#define NUM_COINS 8
+/* Adjusted initialization of the `coins` array to fix the "muitos valores de inicializadores" error.
+   The issue was caused by incorrect nesting of the `ColorRange` struct. */
+
+CoinType coins[NUM_COINS] = {
+    {"1c",  {20, 50, 20, 40, 70, 60}},
+    {"2c",  {20, 50, 20, 40, 70, 60}},
+    {"5c",  {20, 50, 20, 40, 70, 60}},
+    {"10c", {85, 80, 55, 115, 110, 75}},
+    {"20c", {85, 80, 55, 115, 110, 75}},
+    {"50c", {85, 80, 55, 115, 110, 75}},
+    {"1eur",{170, 170, 170, 220, 220, 220}},
+    {"2eur",{100, 100, 100, 150, 150, 150}}
+};
+
+//wompwomp
+
+/** ====================================================== DANGER ZONE ====================================================== */
+
+// Converte imagem RGB para escala de cinza (grayscale)
+void vc_rgb_to_gray(IVC* image, unsigned char* gray) {
+    for (int i = 0; i < image->width * image->height; i++) {
+        int r = image->data[3 * i];
+        int g = image->data[3 * i + 1];
+        int b = image->data[3 * i + 2];
+        gray[i] = (unsigned char)((0.299 * r) + (0.587 * g) + (0.114 * b));
+    }
+}
+
+
+
+/** ========================================================================================================================= */
+
 IVC* vc_image_new(int width, int height, int channels, int levels)
 {
     IVC* image = (IVC*)malloc(sizeof(IVC));
