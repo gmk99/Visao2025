e0306d8da463225763fccaf6f9381677f0774d25
gmk99 <a25664@alunos.ipca.pt>
2025-05-18 18:45:22 +0100

    eliminar 2 antriores

--- Visao2025/vc.c
+++ Visao2025/vc.c
@@ -54,24 +54,3 @@ int vc_rgb_get_green(IVC* image)
     }
     return 1;
 }
-
-
-///teste01
-
-int vc_rgb_get_green(IVC* image)
-{
-    if (image == NULL || image->data == NULL || image->channels != 3)
-        return 0;
-
-    int x, y;
-    for (y = 0; y < image->height; y++)
-    {
-        for (x = 0; x < image->width; x++)
-        {
-            int pos = (y * image->width + x) * 3;
-            image->data[pos + 0] = image->data[pos + 2]; // Red <- Green
-            image->data[pos + 2] = image->data[pos + 1]; // Blue <- Green
-        }
-    }
-    return 1;
-}
