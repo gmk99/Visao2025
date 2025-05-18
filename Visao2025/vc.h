a5bb15c9218e4e5422b855090e44f47119f77f16
FreverZ <49344556+FreverZ@users.noreply.github.com>
2025-05-18 23:36:32 +0100

    Update vc.h

--- Visao2025/vc.h
+++ Visao2025/vc.h
@@ -16,21 +16,26 @@ extern "C"
         int levels;          // Níveis de cinzento por canal (por ex: 256)
     } IVC;
 
+    typedef struct {
+        int x, y;           // Coordenadas do centro
+        int width, height;  // Dimensões do bounding box
+        int area;           // Número de píxeis do blob
+        int perimeter;      // (Você deve calcular)
+        int label;          // Label associado ao blob
+        int circularity;    // Circularidade do blob
+    } OVC;
+
     // Criação de nova imagem
     IVC* vc_image_new(int width, int height, int channels, int levels);
 
     // Libertar memória de imagem
     int vc_image_free(IVC* image);
 
-    // Exemplo de função: extrai o canal verde de uma imagem RGB
-    int vc_rgb_get_green(IVC* image);
 
 
-    // Converte imagem RGB para escala de cinza (1 canal)
     void vc_rgb_to_gray(IVC* image, unsigned char* gray);
-
-    // Aplica filtro Sobel para bordas simples, saída binária em edges
-
+    int vc_gray_to_binary(IVC* src, IVC* dst, int threshold);
+    int vc_gaussian_blur(IVC* src, IVC* dst, int ksize, float sigma);
 
 #ifdef __cplusplus
 }
